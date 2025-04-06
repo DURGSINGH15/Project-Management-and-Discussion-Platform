c50274eb1460fd344f33533901c9b2433227c553
DURGSINGH15 <durgs1007@gmail.com>
2025-04-06 12:01:44 +0530

    Initial commit : installed frontend dependencies + backend setup with MongoDB config

---
+++ src/main/java/com/teammanager/TeamProjectManagerApplication.java
@@ -0,0 +1,13 @@
+package com.teammanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class        TeamProjectManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamProjectManagerApplication.class, args);
+    }
+
+}
